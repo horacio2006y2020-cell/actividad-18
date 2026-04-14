b82698c443b659d405c2204bc48cfc0158b39f01
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-04-13 22:04:51 -0600

    Agregar archivos de proyecto.

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class CuentaBancaria
+{
+    private double saldo;
+
+    public void SetSaldo(double valor)
+    {
+        saldo = valor;
+    }
+
+    public string GetSaldo()
+    {
+        if (saldo > 0)
+            return "Saldo: " + saldo;
+        else
+            return "Saldo no disponible";
+    }
+}
