205921f2ccda5627073720dc6557802b0be5f3f8
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-04-13 22:13:13 -0600

    4

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Empleado
+{
+    private double salario;
+    private const double salarioMinimo = 3000; // puedes cambiarlo
+
+    public void SetSalario(double valor)
+    {
+        salario = valor;
+    }
+
+    public string GetSalario()
+    {
+        if (salario > salarioMinimo)
+            return "Salario: " + salario;
+        else
+            return "Salario inválido";
+    }
+}
