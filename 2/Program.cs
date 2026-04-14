e80ad08455c94650c777936ef3ba68bb52775980
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-04-13 22:08:02 -0600

    2

---
+++ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Estudiante
+{
+    private int edad;
+
+    public void SetEdad(int valor)
+    {
+        edad = valor;
+    }
+
+    public int GetEdad()
+    {
+        if (edad >= 0 && edad <= 100)
+            return edad;
+        else
+            return 0;
+    }
+}
