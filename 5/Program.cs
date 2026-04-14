b3cf514f790b45a96fc75c8b05412f9a7b79a568
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-04-13 22:23:06 -0600

    5

---
+++ 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Usuario
+{
+    private string contraseña = ""; 
+
+    public void SetContraseña(string valor)
+    {
+        contraseña = valor;
+    }
+
+    public string GetContraseña()
+    {
+        if (contraseña.Length >= 8)
+            return "****";
+        else
+            return "Contraseña insegura";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Usuario u = new Usuario();
+        u.SetContraseña("1234");
+        Console.WriteLine(u.GetContraseña());
+    }
+}
