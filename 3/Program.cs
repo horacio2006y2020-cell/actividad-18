9a0d4f96d3ebff6e4bef84dda705729d8d6052c2
horacio2006y2020-cell <horacio2006y2020@gmail.com>
2026-04-13 22:11:02 -0600

    3

---
+++ 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Producto
+{
+    private double precio;
+
+    public void SetPrecio(double valor)
+    {
+        precio = valor;
+    }
+
+    public double GetPrecio()
+    {
+        if (precio >= 0)
+            return precio;
+        else
+            return 0;
+    }
+}
